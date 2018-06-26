508ce2986a04301d80defe51a9fb5ee20a370254
Steven Gurr <3526050+StevenGurr@users.noreply.github.com>
2018-06-26 21:09:56 +0100

    Initial Commit

---
+++ SGTableViewData/SGTableViewData.h
@@ -0,0 +1,19 @@
+//
+//  SGTableViewData.h
+//  SGTableViewData
+//
+//  Created by Steven Gurr on 26/06/2018.
+//  Copyright © 2018 Steven Gurr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGTableViewData.
+FOUNDATION_EXPORT double SGTableViewDataVersionNumber;
+
+//! Project version string for SGTableViewData.
+FOUNDATION_EXPORT const unsigned char SGTableViewDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGTableViewData/PublicHeader.h>
+
+
